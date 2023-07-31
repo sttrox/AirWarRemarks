365a4822a43e1897f55ddcb220639efb6626f4f5
Name <22417706+sttrox@users.noreply.github.com>
2023-07-31 20:48:24 +0000

    Apply gitignore

--- Assets/Scripts/Enemy/Enemy.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[RequireComponent(typeof(Rigidbody2D))]
-public abstract class Enemy : MonoBehaviour
-{
-
-}
--- Assets/Scripts/Enemy/EnemyAttack/EnemyAttack.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public abstract class EnemyAttack : MonoBehaviour
-{
-    [SerializeField] protected int _damage = 1;
-}
